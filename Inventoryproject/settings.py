9eeb25024641d7efeab84408d593e04684438f2b
Jidanel <Jdanielnsas@yahoo.com>
2023-08-11 22:27:32 +0100

    Etape 2 mise en place de nav et base

--- Inventoryproject/settings.py
+++ Inventoryproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Inventoryproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],       #Pour dire à django que le dossier templates a été crée et se trouve dnas le dossier mere du projet
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse ("This is the index page")
+    return render(request, 'index.html')
 
 def staff(request):
 
-    return HttpResponse('<h1 style="color:red;"> This is staff page</h1>')
+    return render(request, 'staff.html')
